4d6575bc30c4ece77400a6dc5ddad5ecd2affe74
Castellanario <castellanario@gmail.com>
2023-01-03 17:09:38 +0100

    Añadir el viewport scale

--- index.php
+++ index.php
@@ -156,6 +156,7 @@ if ($action === 'show-random') {
     <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo $html_title; ?></title>
         <style>
             @font-face {
