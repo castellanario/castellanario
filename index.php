de1a15fdd3845e2f8f0ee57be10e2367be989548
Castellanario <castellanario@gmail.com>
2023-02-27 10:09:07 +0100

    Adsense del aleonsir

--- index.php
+++ index.php
@@ -321,6 +321,8 @@ if ($action === 'show-random') {
             gtag('js', new Date());
             gtag('config', 'G-J2JFKPHKCZ');
         </script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3044831242285145"
+                crossorigin="anonymous"></script>
     </head>
     <body>
 
