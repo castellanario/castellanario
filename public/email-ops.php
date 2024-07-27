d65be6242984fbbb90a2f371b03ea3787a332ec1
Alvaro Franz <mail@alvarofranz.com>
2024-07-27 12:12:18 +0200

    borra tambien la imagen si se ha llegado a generar

--- public/email-ops.php
+++ public/email-ops.php
@@ -22,6 +22,10 @@ switch($_GET['action']) {
         break;
     case 'delete-this-sht':
         $query = "DELETE FROM `castellanario` WHERE `id` = $id";
+        $img = __DIR__ . '/images/' . $id . '.jpg';
+        if (file_exists($img)) {
+            unlink($img);
+        }
         $msg_if_ok = 'i deleted this shit for you';
         break;
     default:
